01f8e65842aad185dabf489dcba53468cccbf1bc
Dr Jauregui <albertodomjauregui@hotmail.com>
2020-12-12 10:46:17 +0100

    modify way of generating products seed

--- knex/seeds/02_products.js
+++ knex/seeds/02_products.js
@@ -1,32 +1,18 @@
 escape_quotes = require("escape-quotes");
 
 exports.seed = function (knex, Promise) {
-  let products = [
-    {
-      id: 1,
-      name: "Lejía El Arroyo 2L",
+  let products = [];
+  const numberOfRegisters = 60;
+  for (var i = 0; i < numberOfRegisters; i++) {
+    products.push({
+      id: i + 1,
+      name: `Producto ${i}`,
       cost: 0.12,
       green_point_amount: 0.0125,
       units_per_box: 12,
       capacity: 2,
-    },
-    {
-      id: 2,
-      name: "Fregasuelos El arroyo Limon 1.5L",
-      cost: 0.25,
-      green_point_amount: 0.0126,
-      units_per_box: 15,
-      capacity: 1.5,
-    },
-    {
-      id: 3,
-      name: "MultiUsos Mi Hada 1.5L",
-      cost: 0.25,
-      green_point_amount: 0.0129,
-      units_per_box: 15,
-      capacity: 1.5,
-    },
-  ];
+    });
+  }
 
   return knex("products").insert(products);
 };
