7e3a0c525bd8da1cc40a246db0dfaeb96b9f216f
Dr Jauregui <albertodomjauregui@hotmail.com>
2020-12-15 18:40:45 +0100

    few fakes fares

--- knex/seeds/05_fares.js
+++ knex/seeds/05_fares.js
@@ -7,19 +7,25 @@ exports.seed = function (knex, Promise) {
   return knex("customers").then((customers) => {
     return knex("products").then((products) => {
       customers.forEach((customer) => {
-        for (var i = 0; i < 3; i++) {
-          const randomProduct = utils.getRandomItemOfArray(products);
-          fares.push({
-            customer_id: customer.id,
-            customer_name: customer.name,
-            product_id: randomProduct.id,
-            price_1: faker.random.number(3),
-            price_2: faker.random.number(3),
-            price_3: faker.random.number(3),
-            price_4: faker.random.number(3),
-            to_sell: 6,
-            to_charge: 5,
-          });
+        const isCustomerIncludedInList = [1, 2, 3, 4, 5, 6, 90].includes(
+          customer.id
+        );
+        if (isCustomerIncludedInList) {
+          //CREATE A RANDOM LIST OF DIFFERENT FARES ONLY FOR A FEW CUSTOMERS AND NOT FOR ALL, NOT TO COLLAPSE THE TABLE
+          for (var i = 0; i < 3; i++) {
+            const randomProduct = utils.getRandomItemOfArray(products);
+            fares.push({
+              customer_id: customer.id,
+              customer_name: customer.name,
+              product_id: randomProduct.id,
+              price_1: faker.random.number(3),
+              price_2: faker.random.number(3),
+              price_3: faker.random.number(3),
+              price_4: faker.random.number(3),
+              to_sell: 6,
+              to_charge: 5,
+            });
+          }
         }
       });
       return knex("fares").insert(fares);
