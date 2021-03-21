44e4e8e541cd133d03fe5a524224726bed725e36
Dr Jauregui <albertodomjauregui@hotmail.com>
2021-03-21 19:19:15 +0100

    edit product

--- routes/products.js
+++ routes/products.js
@@ -22,4 +22,21 @@ router.get("/:id?", function (req, res, next) {
     .catch((err) => res.status(500).json({ error: err.sqlMessage }));
 });
 
+router.put("/:productId", function (req, res, next) {
+  const { body } = req;
+  const { productId: id } = req.params;
+  const product = body;
+
+  return knex
+    .transaction(async function (trx) {
+      await trx("products").where({ id }).update(product);
+    })
+    .then(function () {
+      res.send({ success: true, info: "Product edited successfully" });
+    })
+    .catch(function (e) {
+      res.status(500).json({ success: false, info: "Something went wrong" });
+    });
+});
+
 module.exports = router;
